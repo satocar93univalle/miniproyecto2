2dd1ede5f01ef65a714d25f8c45537568d0188fa
santiago <santiago.torres@correounivalle.edu.co>
2022-06-13 22:24:29 -0500

    Se diseñaron los patrones de imagenes y se agregaron al proyecto

--- src/logica/Coordenada.java
+++ src/logica/Coordenada.java
@@ -9,5 +9,29 @@ package logica;
  * @author Santiago
  */
 public class Coordenada {
+    private int x;
+    private int y;
+
+    public Coordenada(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
     
 }
